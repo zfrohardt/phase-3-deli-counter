8c1670e249e7505faa33954803f8231f07b81b1c
Zach Frohardt <zfrohardt@gmail.com>
2021-05-17 11:50:38 -0700

    completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def line(line)
+    if line.length == 0
+        puts "The line is currently empty."
+    else
+        puts line.map.with_index {|e, i| "#{i + 1}. #{e}"}.unshift("The line is currently:").join(" ")
+    end
+end
+
+def take_a_number(line, person)
+    line.push person
+    puts "Welcome, #{person}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+    if line.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{line.shift}."
+    end
+end
